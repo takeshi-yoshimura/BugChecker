2d1f1e7d369946dd4cac1bec4657f40c96539d46
Takeshi Yoshimura <yos@sslab.ics.keio.ac.jp>
2015-05-28 14:43:53 +0000

    add platform_drivers and i2c_drivers

--- scripts/instrument.py
+++ scripts/instrument.py
@@ -2,6 +2,8 @@
 import sys, os, shutil
 from collections import defaultdict
 
+target_struct = ["pci_driver", "dev_pm_ops", "pci_error_handlers", "platform_driver", "i2c_driver"]
+
 pm_callback_prefix = "wrapper_pm_"
 pm_callbacks = {
 "prepare": ["int", ["struct device *", "dev"]],
@@ -45,6 +47,23 @@ pci_error_handlers_callbacks = {
 "resume": ["void", ["struct pci_dev *", "dev"]],
 }
 
+platform_callback_prefix = "wrapper_platform_driver_"
+platform_callbacks = {
+"probe": ["int", ["struct platform_device *", "dev"]],
+"remove": ["void", ["struct platform_device *", "dev"]],
+"suspend": ["int", ["struct platform_device *", "dev"], ["pm_message_t", "state"]],
+"resume": ["int", ["struct platform_device *", "dev"]],
+"shutdown": ["void", ["struct platform_device *", "dev"]],
+}
+
+i2c_callback_prefix = "wrapper_i2c_driver_"
+i2c_callbacks = {
+"probe": ["int", ["struct i2c_client *", "cl"], ["const struct i2c_device_id *", "id"]],
+"remove": ["void", ["struct i2c_client *", "cl"]],
+"shutdown": ["void", ["struct i2c_client *", "cl"]],
+}
+
+
 def printStructFunc(callbacks, opsForFile, prefix):
     outStr = ""
     for structFunc in callbacks.keys():
@@ -65,9 +84,7 @@ def printStructFunc(callbacks, opsForFile, prefix):
                 outStr += "\treturn " + callStr + "\n"
             else:
                 outStr += "\t" + callStr + "\n"
-            if structFunc == "shutdown":
-                print "DEBUG: pci_driver::shutdown == " + opsForFile["shutdown"]
-        elif callbacks == pci_callbacks and structFunc == "shutdown" and opsForFile.has_key("remove"):
+        elif structFunc == "shutdown" and opsForFile.has_key("remove"):
             callStr = opsForFile["remove"] + "("
             for arg in args[:-1]:
                 callStr += args[1] + ", "
@@ -83,7 +100,7 @@ def printStructFunc(callbacks, opsForFile, prefix):
 
 
 if len(sys.argv) != 4:
-    print "specify (linux dir) (pci model file) (outputdir)"
+    print "specify (linux dir) (driver_life.c) (getentryexit subdir)"
     exit(1)
 
 linux_dir = sys.argv[1]
@@ -97,6 +114,11 @@ f.close()
 pci_drivers = defaultdict(dict)
 dev_pm_ops = defaultdict(dict)
 pci_error_handlers = defaultdict(dict)
+platform_drivers = defaultdict(dict)
+i2c_drivers = defaultdict(dict)
+
+files = set([])
+
 for line in lines:
     line_split = line.strip().split('\t')
     type = line_split[0]
@@ -110,33 +132,48 @@ for line in lines:
     struct = type[:type.find("::")]
     structFunc = type[type.find("::") + 2:]
     
+    if not struct in target_struct:
+        continue
+    
     file = linux_dir + "/" + func[:func.rfind("/")] + ".c"
     callback = func[func.rfind("/") + 1:]
-    
-    if struct != "pci_driver" and struct != "dev_pm_ops" and struct != "pci_error_handlers":
-        continue
+
+    files.add(file)
     if struct == "pci_driver":
         pci_drivers[file][structFunc] = callback
-    if struct == "dev_pm_ops":
+    elif struct == "dev_pm_ops":
         dev_pm_ops[file][structFunc] = callback
-    if struct == "pci_error_handlers":
+    elif struct == "pci_error_handlers":
         pci_error_handlers[file][structFunc] = callback
+    elif struct == "platform_driver":
+        platform_drivers[file][structFunc] = callback
+    elif struct == "i2c_driver":
+        i2c_drivers[file][structFunc] = callback
 
 f = open(model_file, "r")
 scenarioStr = f.read()
 f.close()
 
 f2 = open(output_dir + "/build_target.txt", "w")
-for file in pci_drivers.keys():
-    defineStr = "#define TEST_PCI_DRIVER\n"
-    outStr = printStructFunc(pci_callbacks, pci_drivers[file], pci_callback_prefix)
-    if dev_pm_ops.has_key(file):
-        defineStr += "#define TEST_PM\n"
-        outStr += printStructFunc(pm_callbacks, dev_pm_ops[file], pm_callback_prefix)
-    if pci_error_handlers.has_key(file):
-        defineStr += "#define TEST_PCI_ERROR_HANDLERS\n"
-        outStr += printStructFunc(pci_error_handlers_callbacks, pci_error_handlers[file], pci_error_handlers_callback_prefix)
- 
+for file in files:
+    defineStr = ""
+    outStr = ""
+    if pci_drivers.has_key(file):
+        defineStr += "#define TEST_PCI_DRIVER\n"
+        outStr += printStructFunc(pci_callbacks, pci_drivers[file], pci_callback_prefix)
+        if dev_pm_ops.has_key(file):
+            defineStr += "#define TEST_PM\n"
+            outStr += printStructFunc(pm_callbacks, dev_pm_ops[file], pm_callback_prefix)
+        if pci_error_handlers.has_key(file):
+            defineStr += "#define TEST_PCI_ERROR_HANDLERS\n"
+            outStr += printStructFunc(pci_error_handlers_callbacks, pci_error_handlers[file], pci_error_handlers_callback_prefix)
+    if platform_drivers.has_key(file):
+        defineStr += "#define TEST_PLATFORM_DRIVER\n"
+        outStr += printStructFunc(platform_callbacks, platform_drivers[file], platform_callback_prefix)
+    if i2c_drivers.has_key(file):
+        defineStr += "#define TEST_I2C_DRIVER\n"
+        outStr += printStructFunc(i2c_callbacks, i2c_drivers[file], i2c_callback_prefix)
+
     f = open(file, "a")
     f.write("\n/* the below section is automatically added by instrument.py */\n\n")
     f.write("#ifdef __clang_analyzer__\n\n")
